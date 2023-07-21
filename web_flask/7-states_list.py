56b4ad94e17f9375ea72e602de2dac83cff7d5ed
Bruno Njeru <118963661+bmn44100@users.noreply.github.com>
2023-07-21 22:41:52 +0300

    a script that starts a Flask web application

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+""" starts a Flask web application """
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+flaskapp = Flask(__name__)
+
+
+@flaskapp.route('/', strict_slashes=False)
+def display_hello_flask():
+    return 'Hello HBNB!'
+
+
+@flaskapp.route('/hbnb', strict_slashes=False)
+def display_flask():
+    return 'HBNB'
+
+
+@flaskapp.route('/c/<text>', strict_slashes=False)
+def display_c_is_fun(text):
+    return 'C %s' % text.replace("_", " ")
+
+
+@flaskapp.route('/python', strict_slashes=False)
+def display_python_is():
+    return 'Python is cool'
+
+
+@flaskapp.route('/python/<text>', strict_slashes=False)
+def display_python_is_magic(text='is_cool'):
+    return 'Python %s' % text.replace("_", " ")
+
+
+@flaskapp.route('/number/<int:n>', strict_slashes=False)
+def display_int_only(n):
+    return '%d is a number' % n
+
+
+@flaskapp.route('/number_template/<int:n>', strict_slashes=False)
+def display_template(n=None):
+    return render_template('5-number.html', n=n)
+
+
+@flaskapp.route('/number_odd_or_even/<int:n>')
+def display_odd_or_even(n):
+    if n % 2 == 0:
+        num = 'even'
+    else:
+        num = 'odd'
+    return render_template('6-number_odd_or_even.html', n=n, num=num)
+
+
+@flaskapp.route('/states_list', strict_slashes=False)
+def display_states():
+    states = storage.all(State)
+    print(states)
+    return render_template('7-states_list.html', states=states)
+
+
+@flaskapp.teardown_appcontext
+def display_teardown(exeption):
+    storage.close()
+
+
+if __name__ == "__main__":
+    flaskapp.run(host="0.0.0.0")
